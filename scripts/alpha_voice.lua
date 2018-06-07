93fe2a1667ae4b23b4bd469a070e862c35e77011
Shirsh <theshirsh@gmail.com>
2018-06-08 05:55:53 +0700

    + Revived speech

--- scripts/alpha_voice.lua
+++ scripts/alpha_voice.lua
@@ -117,21 +117,50 @@ local alpha_voice =
 	onEventTrigger = function( self, sim, evType, evData, before )	
 		local script = sim:getLevelScript()
 		local agentDef = self.abilityOwner:getUnitData()
-		
+
+	-- Pinning block:	
+	
 		if evType == simdefs.EV_UNIT_STOP_WALKING and (evData.unit == self.abilityOwner or evData.unitID == self.abilityOwner:getID()) and simquery.isUnitPinning( sim, evData.unit ) and not before then
 			sim:dispatchEvent( simdefs.EV_UNIT_START_PIN, evData )
 		end
-		
-		if evType == simdefs.EV_UNIT_HEAL and evData.revive and not before then
-    			if evData.unit == self.abilityOwner then   
-				evType = 1100;			-- custom number added for using medgel on other agent
-				--is the necromancer
-  			elseif evData.target == self.abilityOwner then
-				evType = 102;			-- normal revived trigger
-        			--is the lazarus
-			end
+
+	-- Block for 'passive' events(being target of healing), triggers after:
+
+		if (evData.target == self.abilityOwner or evData.targetID == self.abilityOwner:getID()) and not before then 
+			if evType == simdefs.EV_UNIT_HEAL and evData.revive and not before then	    			
+				if agentDef.agentID ~= nil then 
+					local agent = agentDef.agentID	
+					if agent == 99 then					-- last mission's Monster to starting Monster 
+						agent = 100;
+					elseif agent == 107 then 				-- last mission's Central to starting Central 
+						agent = 108;
+					end
+					if STRINGS.alpha_voice[ agent] ~= nil then		
+						local speechData = STRINGS.alpha_voice[ agent][evType ]				
+						if speechData ~= nil then			
+							local p = speechData[1]
+							if sim:nextRand() <= p then
+						   		local choice = speechData[2]
+								local speech = choice[math.floor(sim:nextRand()*#choice)+1]								
+								local text =  {{							
+									text = speech,
+									anim = agentDef.profile_anim,
+									build = agentDef.profile_build,
+									name = agentDef.name,
+									timing = 3,
+									voice = nil,
+								}}					
+								script:queue( { script=text, type="newOperatorMessage", doNotQueue=true } ) 
+								--script:queue( 3*cdefs.SECONDS )
+								--script:queue( { type="clearOperatorMessage" } ) -- it autoclears after "timing =3" I think
+							end
+						end
+					end
+				end
+			end			
 		end
-		
+	
+	-- Block for 'active' events(being executor of action of healing etc):	
 		
 		if (evData.unit == self.abilityOwner or evData.unitID == self.abilityOwner:getID()) and not evData.cancel and before then 	
 			if not self.abilityOwner:isKO() then
@@ -142,6 +171,9 @@ local alpha_voice =
 					  end
 				elseif evType == simdefs.EV_UNIT_WIRELESS_SCAN then		-- redirects Int's wireless hijack
 					evType = 19;	
+				elseif evType == simdefs.EV_UNIT_HEAL and evData.revive then
+					evType = 1009;						-- custom number added for using medgel on other agent
+					--is the necromancer
 				end
 				if agentDef.agentID ~= nil then 
 					local agent = agentDef.agentID	
